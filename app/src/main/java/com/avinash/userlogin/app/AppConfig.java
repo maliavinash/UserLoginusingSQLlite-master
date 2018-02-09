fab56e4030c1243a8469766f8c1de3be7a88e1cc
maliavinash <avi24.9@gmail.com>
2018-02-09 12:44:12 +0530

    Initial commit

---
+++ app/src/main/java/com/avinash/userlogin/app/AppConfig.java
@@ -0,0 +1,14 @@
+package com.avinash.userlogin.app;
+
+/**
+ * Created by Avinash on 19-01-2018.
+ */
+
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.0.112/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.112/android_login_api/register.php";
+}
